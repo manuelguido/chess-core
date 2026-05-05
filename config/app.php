d1e871db562c57afd9f46ab700c7968d3d81a485
Manuel Guido <manuelguido.gb@gmail.com>
2026-05-05 19:14:33 -0300

    fix: update project name from Chess Studio to Chess Core in configuration files

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Chess Studio'),
+    'name' => env('APP_NAME', 'Chess Core'),
 
     /*
     |--------------------------------------------------------------------------
